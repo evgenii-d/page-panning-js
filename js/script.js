a676c83dd1f5026865a50ebbe0a1c2407cad51a6
Evgenii Dolgii <zerson13@gmail.com>
2022-10-23 12:22:15 +0300

    Add comments for init script

--- js/script.js
+++ js/script.js
@@ -7,14 +7,21 @@ function touchHandler(e) { e.touches.length > 1 ? e.preventDefault() : '' }
 window.addEventListener('contextmenu', e => e.preventDefault())
 window.addEventListener('touchstart', touchHandler, { passive: false })
 
+// append svg mask to container
 mainContainer.insertAdjacentHTML('beforeend', SVG_MASK)
+
+// get all clickable areas from svg mask
 activeElements = mainContainer.querySelectorAll('svg [data-id]')
+
+// add desirable behaviour for clickable areas
 activeElements.forEach(e => { e.addEventListener('click', clickHandler) })
 
+// config popup
 'timeout' in DATA ? mediaPopup.timeout(DATA.timeout) : ''
 mediaPopup.onOpen(() => { pagePanning.pause() })
 mediaPopup.onClose(() => { pagePanning.play() })
 
+// config page panning
 if ('pagePanning' in DATA) {
     DATA.pagePanning.delay ? pagePanning.delay(DATA.pagePanning.delay) : ''
     DATA.pagePanning.step ? pagePanning.step(DATA.pagePanning.step) : ''
